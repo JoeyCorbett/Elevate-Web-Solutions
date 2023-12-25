d4eadf1d9a04fc202799aab74983369bbce0d324
JoeyCorbett <corbettjoey9@gmail.com>
2023-12-25 13:22:33 -0500

    Add styling to sign up form and add animation to sign in when opened
    Made the sign up form smaller and added a fade in animation which slides down from the top

--- index.js
+++ index.js
@@ -16,24 +16,28 @@ document.addEventListener("DOMContentLoaded", function () {
   // Open Modal when clicked
   SignUpButton.onclick = function () {
     SignUpModal.style.display = "flex";
+    // Enable animation
+    SignUpModal.classList.add('modal-animate');
     backdrop.style.display = "flex";
     // Disable scrolling on page
-    document.body.classList.add('no-scroll');
+    document.body.classList.add("no-scroll");
   };
 
   // Close Modal when exit pressed
   CloseButton.onclick = function () {
     SignUpModal.style.display = "none";
     backdrop.style.display = "none";
+    // Disable animation
+    SignUpModal.classList.remove('show');
     // Enable scrolling on page
-    document.body.classList.remove('no-scroll');
+    document.body.classList.remove("no-scroll");
   };
 
-  document.getElementById('modalBackdrop').onclick = function(event) {
+  document.getElementById("modalBackdrop").onclick = function (event) {
     if (event.target == this) {
-      this.style.display = 'none';
+      this.style.display = "none";
       // Enable scrolling on page
-      document.body.classList.remove('no-scroll');
+      document.body.classList.remove("no-scroll");
     }
-  }
+  };
 });
